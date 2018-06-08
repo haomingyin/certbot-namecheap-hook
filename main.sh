a14f743ccb4006ba02ed6597022243a3fa64d2c4
Haoming Yin <haoming.yin@xero.com>
2018-06-08 22:14:39 +1200

    Auto load local IP address to CLIENT_IP

--- main.sh
+++ main.sh
@@ -7,7 +7,13 @@
 # export CLIENT_IP=127.0.0.1
 # export SLD=haomingyin
 # export TLD=com
+# export APPLY_DOMAIN=*.haomingyin.com
 
+# if CLIENT_IP is not set, then local IP will be used
+if [ -z "$CLIENT_IP" ]; then
+    localIp=$IP && [[ -z $IP ]] && localIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+    export CLIENT_IP=localIp
+fi
 
 # certbot renew \
 # --logs-dir /usr/local/var/log/letsencrypt \
@@ -27,7 +33,7 @@ certbot certonly \
 --preferred-challenges=dns \
 --manual-auth-hook ./authenticator.sh \
 --manual-cleanup-hook ./cleanup.sh \
--d *.haomingyin.com \
+-d $APPLY_DOMAIN \
 --server https://acme-v02.api.letsencrypt.org/directory \
 --manual-public-ip-logging-ok \
 --force-renewal
